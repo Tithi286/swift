1c8f37f58ac73cb1a9915c0032e61473ac28175b
Michael Hsieh <mdhsieh8@gmail.com>
2019-10-03 20:22:34 -0700

    can now do slash command /add-job-opening as example

--- src/api/controllers/commands.js
+++ src/api/controllers/commands.js
@@ -1,13 +1,24 @@
 import express from 'express'
+import { postJobOpening } from '../services/slack'
 
 const router = new express.Router()
 
 router.all('/commands', async (req, res) => {
-    console.log(req.body.command)
+    /*console.log(req.body.command)
     
     console.log("command request body is ", req.body)
 
-    res.status(200).send('Thanks.')
+    res.status(200).send('Thanks.')*/
+    
+    let status = 200
+    await postJobOpening(process.env.jobs_channel, req.body.text)
+        .catch(() => {
+            status = 503
+        })
+    res.status(status).send('Slash command completed.')
+    
+    console.log("command request body is")
+    console.log(req.body)
 })
 
 export default router
