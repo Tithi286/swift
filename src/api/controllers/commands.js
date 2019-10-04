9c4d9684556353d78a43bbd72b5c830ed16d2187
Michael Hsieh <mdhsieh8@gmail.com>
2019-10-03 17:26:06 -0700

    added .env config file, yarn slack, and dotenv

--- src/api/controllers/commands.js
+++ src/api/controllers/commands.js
@@ -4,6 +4,8 @@ const router = new express.Router()
 
 router.all('/commands', async (req, res) => {
     console.log(req.body.command)
+    
+    console.log("command request body is ", req.body)
 
     res.status(200).send('Thanks.')
 })
