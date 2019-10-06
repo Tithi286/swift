1269f5601574139cb3b2353954dbd765c331feb2
Michael Hsieh <mdhsieh8@gmail.com>
2019-10-05 18:31:36 -0700

    can now console log channel history

--- src/api/controllers/commands.js
+++ src/api/controllers/commands.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { postJobOpening } from '../services/slack'
+//import { postJobOpening } from '../services/slack'
+import { forward } from '../services/slack'
 
 const router = new express.Router()
 
@@ -9,9 +10,16 @@ router.post('/commands', async (req, res) => {
     console.log("command request body is ", req.body)
 
     res.status(200).send('Thanks.')*/
-    
+
+    /*let status = 200
+    await forward(process.env.jobs_channel, req.body.text)
+        .catch(() => {
+            status = 503
+        })
+    res.status(status).send('Slash command completed.')*/
+
     let status = 200
-    await postJobOpening(process.env.jobs_channel, req.body.text)
+    let result = await forward(process.env.jobs_channel)
         .catch(() => {
             status = 503
         })
@@ -19,6 +27,8 @@ router.post('/commands', async (req, res) => {
     
     console.log("command request body is")
     console.log(req.body)
+    console.log('result is')
+    console.log(result)
 })
 
 export default router
--- src/api/services/slack.js
+++ src/api/services/slack.js
@@ -1,3 +1,4 @@
 import slack from 'slack'
 
-module.exports.postJobOpening = (channel, text) => slack.chat.postMessage({token: process.env.slack_app_token, channel, text})
+// module.exports.forward = (channel, text) => slack.chat.postMessage({token: process.env.slack_app_token, channel, text})
+module.exports.forward = (channel) => slack.channels.history({token: process.env.slack_app_token, channel})
